44f6cbd7525b0459a71655150599a6c2f232fa70
Ebrahem Eltemsahy <ebrahem.eltemsahy@gmail.com>
2025-05-12 02:04:38 +0300

    Add files via upload

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "../components/Navbar";
+import { Dropdown, Card, CardGroup, Carousel, Ratio } from 'react-bootstrap';
+
+const Admin = () => {
+  <Navbar />
+  return (
+    
+    <div>
+      
+    </div>
+  )
+}
+
+export default Admin
